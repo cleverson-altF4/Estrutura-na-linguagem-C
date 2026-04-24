78924183d1cb3c1f9e79f6de68326cca024d3e8d
Clevison <cleversonmaiden034@gmail.com>
2026-04-24 16:20:15 -0500

    Sistema de altitude

---
+++ aula-3.c
@@ -0,0 +1,19 @@
+/*Sistema de Altitude
+
+Crie:
+
+altitude atual (int)
+altitude máxima permitida (int)*/
+
+#include <stdio.h>
+
+int main(){
+    printf("Altitude atual\n");
+    int altitude_atual = 15;
+    int altitude_permitida = 13;
+
+    printf("Altitude atual: %d", altitude_atual);
+    printf("Altitude permitida: %d\n", altitude_permitida);
+
+    return 0;
+}
