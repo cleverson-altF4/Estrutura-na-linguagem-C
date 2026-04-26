e649daa64201abe987a103687fc06f682252ec27
Clevison <cleversonmaiden034@gmail.com>
2026-04-26 17:17:15 -0500

    Controle

---
+++ aula-9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int motor1 = 50;
+    int motor2 = 50;
+    int motor3 = 50;
+    int motor4 = 50;
+
+    printf("Motor 1: %d%%\n", motor1);
+    printf("Motor 2: %d%%\n", motor2);
+    printf("Motor 3: %d%%\n", motor3);
+    printf("Motor 4: %d%%\n", motor4);
+
+    return 0;
+}
