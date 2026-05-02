0ce0263add77a6563b4c99c5026b4b8d7f4e9c96
Clevison <cleversonmaiden034@gmail.com>
2026-05-02 11:42:52 -0500

    Desafio feito

--- desafio-1.c
+++ desafio-1.c
@@ -8,8 +8,38 @@ void ligar_motores(){
     for (int i = 0; i < 4; i++)
     {
         motores[i] = 30;
-    }   
+    } 
+    
+    
 }
-int main(){
 
+void exibir_status(){
+    printf("==== Status do Drone ====\n");
+    printf("Bateria: %.1f%%\n", bateria);
+    printf("Altitude: %.1fM\n", altitude);
+    for (int i = 0; i < 4; i++)
+    {
+        printf("Motor %d : %d%%\n", i+1, motores[i]);
+    }
+    printf("==========================\n");
+    
+}
+
+void subir(float metros){
+    altitude = altitude + metros;
+}
+
+void consumir_bateria(){
+    bateria = bateria - 10;
+}
+
+int main(){
+    ligar_motores();
+    subir(15);
+    consumir_bateria();
+    consumir_bateria();
+    consumir_bateria();
+    exibir_status();
+
+    return 0;
 }
